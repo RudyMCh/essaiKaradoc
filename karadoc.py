a7ae1b902688ad98e03410c1aeeeecfc44e41d56
Rudy Masson <rmasson.pro@gmail.com>
2019-02-22 19:52:47 +0100

    correction fautes d'orthographe

--- karadoc.py
+++ karadoc.py
@@ -6,6 +6,6 @@ Created on Wed Feb 20 17:43:21 2019
 """
 
 if __name__ == "__main__":
-    print("Les chicots, c'est sacré ! Parce que si j'les lave pas maintenant, dans dix ans, c'est tout à la soupe. Et l'mec qui me fera manger de la soupe il est pas né !")
+    print("Les chicots, c'est sacré ! En effet, si je ne les lave pas ce sera la merde .Parce que si j'les lave pas maintenant, dans dix ans, c'est tout à la soupe. Et l'mec qui me fera manger de la soupe il est pas né !")
 else:
     print('Du passé faisons table en marbre.')
